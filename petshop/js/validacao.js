c80abee5d5b943dc5f465ed0cc8ac7ed366cb0e4
PamelaLimaa <layanepamela98@gmail.com>
2022-07-19 13:37:32 -0300

    atualizacoes do projeto

--- petshop/js/validacao.js
+++ petshop/js/validacao.js
@@ -13,10 +13,21 @@ dataNascimento.addEventListener('blur', (evento) => {
 
     if(input,validity.valid){
         input.parentElement.classList.remove('input-container--invalido')
+        input.parentElement.querySelector('input-mensagem-erro').innerHTML = ''
     }else{
         input.parentElement.classList.add('input-container--invalido')
+        input.parentElement.querySelector('input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input)
+
     }
 }
+
+
+const tiposDeErro= [
+    'valueMissing',
+    'typeMismatch',
+   'patternMissing',
+    'customError'
+]
 
 const mensagensDeErro = {
     nome: {
@@ -39,6 +50,17 @@ const mensagensDeErro = {
 const validadores = {
     dataNascimento: input => validaDataNascimento(input )
 } 
+
+function mostraMensagemDeErro(tipoDeInput, input) {
+    let mensagem =''
+
+    tiposDeErro.forEach(erro =>{
+        if(input.validity[erro]){
+            mensagem = mensagensDeErro[tipoDeInput][erro]
+        }
+    })
+    return mensagem
+}
 
 function validaDataNascimento(input){
     const dataRecebida = new Date(input.value)
